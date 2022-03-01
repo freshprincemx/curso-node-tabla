48a7c4a15b77f1846c291b54b3909ed4dea301c3
Sinhue González <sinhue.glez@gmail.com>
2022-03-01 14:25:50 -0600

    Primer commit

---
+++ helpers/multiplicar.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const colors = require('colors');
+
+const crearArchivo = async (base = 5, listar = false, hasta=10) => {
+    try{
+        let salida = '';
+        let consola = '';
+        for (let i = 1; i<=hasta; i++){
+            consola += `${base} ${'x'.green}  ${i} ${'='.green} ${base * i} \n`;
+            salida += `${base} x ${i} = ${base * i} \n`;
+        }
+        if(listar){
+            console.clear();
+            console.log('========================='.green);
+            console.log('     Tabla del:'.green, colors.blue(base));
+            console.log('========================='.green);
+            console.log(consola);
+        }
+        fs.writeFileSync(`./salida/tabla-${base}.txt`, salida);
+        return `tabla-${base}.txt`;
+    }catch(error){
+        throw error;
+    }
+}
+
+module.exports = {
+    crearArchivo
+};
